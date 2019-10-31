40958beac51c5d9bd0c4f74c5218469a4c30fcb1
KryptosRepublic <fisher_phisher@protonmail.com>
2019-10-31 19:31:14 -0400

    Initial Commit

---
+++ Sense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sense
+//
+//  Created by Bob Yuan on 2019/10/31.
+//  Copyright © 2019 Bob Yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
