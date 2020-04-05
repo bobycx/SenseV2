11884adb511e70a7ef8e5db25ccec0bb93904ffc
Tim <tyuan@hotmail.com>
2020-04-04 17:44:29 -0700

    change cell view class

--- Sense/Model/CellView.swift
+++ Sense/Model/CellView.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class CellView: UIView {
 
-    @IBOutlet weak var firstLabel: UILabel!
-    @IBOutlet weak var secondLabel: UILabel!
     @IBOutlet weak var ansButton: UIButton!
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    @IBOutlet weak var secondLabel: UILabel!
+    
+
+
+    
+    
     @IBOutlet weak var cellView: UIView!
+    
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -21,5 +30,24 @@ class CellView: UIView {
         // Drawing code
     }
     */
-
+  /*使用懒加载,这样XIB的初始化只有在addSubview(view)时才会触发, 而此时xib类肯定已经初始化完毕了*/
+    lazy var view: UIView = {
+        return Bundle.main.loadNibNamed("CellView", owner: self, options: nil)?.first as! UIView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        view.frame = bounds
+        addSubview(view)
+        print("cellView initialized!")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        view.frame = bounds
+        addSubview(view)
+        print("cellView? initialized!")
+    }
 }
+    
+
