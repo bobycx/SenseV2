248f78bfc3629b42992542e6e3d67436867ae7aa
Tim <tyuan@hotmail.com>
2020-04-07 14:54:56 -0700

    recoding

--- Sense/Model/CellView.swift
+++ Sense/Model/CellView.swift
@@ -6,22 +6,19 @@
 //  Copyright © 2020 Bob Yuan. All rights reserved.
 //
 
+// !!!
+// Recoding for:  set the file's owner to "CellView" and leave View as a UIView.
+// By Tim on April 5th, 2020
+// !!!
 import UIKit
+
 
 class CellView: UIView {
 
-    @IBOutlet weak var ansButton: UIButton!
-    
     @IBOutlet weak var firstLabel: UILabel!
-    
+    @IBOutlet var cellView: UIView!
+    @IBOutlet weak var ansButton: UIButton!
     @IBOutlet weak var secondLabel: UILabel!
-    
-
-
-    
-    
-    @IBOutlet weak var cellView: UIView!
-    
     
     /*
     // Only override draw() if you perform custom drawing.
@@ -30,24 +27,57 @@ class CellView: UIView {
         // Drawing code
     }
     */
-  /*使用懒加载,这样XIB的初始化只有在addSubview(view)时才会触发, 而此时xib类肯定已经初始化完毕了*/
-    lazy var view: UIView = {
-        return Bundle.main.loadNibNamed("CellView", owner: self, options: nil)?.first as! UIView
-    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        view.frame = bounds
-        addSubview(view)
-        print("cellView initialized!")
+        self.loadView()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        view.frame = bounds
-        addSubview(view)
-        print("cellView? initialized!")
     }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.loadView()
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.cellView.frame = self.bounds
+    }
+    
+    // getXibName
+    private func getXibName() -> String {
+        let clzzName = NSStringFromClass(self.classForCoder)
+        let nameArray = clzzName.components(separatedBy: ".")
+        var xibName = nameArray[0]
+        if nameArray.count == 2 {
+            xibName = nameArray[1]
+        }
+        return xibName
+    }
+    
+    // Loadview
+    func loadView() {
+        if self.cellView != nil {
+            return
+        }
+        //print("loadViewing...")
+        self.cellView = self.loadViewWithNibName(fileName: self.getXibName(), owner: self)
+        
+        self.cellView.frame = self.bounds // Important!!!
+        
+        self.cellView.backgroundColor = UIColor.clear
+        self.addSubview(self.cellView)
+    }
+    // xib for creating the objects of the CellView
+    private func loadViewWithNibName(fileName: String, owner: AnyObject) -> UIView {
+        let nibs = Bundle.main.loadNibNamed(fileName, owner: owner, options: nil)
+        return nibs?[0] as! UIView
+    }
+    
 }
     
 
